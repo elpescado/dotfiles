58cb3d7f2b3a60d9e4a27eba01f5546083197064
Przemysław Sitek <the.programming.trout@gmail.com>
2022-05-31 11:50:36 +0200

    fish: Cleanup

--- config.fish
+++ config.fish
@@ -1,4 +1,9 @@
+# Prompt
 starship init fish | source
-set -a fish_user_paths $HOME/bin
+
+# Paths
+fish_add_path $HOME/bin
+
+# Colors
 set -gx JQ_COLORS "38;5;246:38;5;196:38;5;70:38;5;214:38;5;27::"
 set -gx CLICOLOR 1
